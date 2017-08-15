4a367c29ad19a75aa8852b48a5ed0c61a81415d5
ztiantian <243752773@qq.com>
2017-08-15 11:34:40 +0800

    8/15

---
+++ app.js
@@ -0,0 +1,17 @@
+var express=require('express');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path=require('path');
+server.listen(80);
+
+app.get('/', function (req, res) {
+  // res.sendfile(__dirname + '/index.html');
+});
+app.use(express.static(path.join(__dirname,'')));
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
